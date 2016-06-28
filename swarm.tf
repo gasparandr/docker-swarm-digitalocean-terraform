fdfa70e779bd58c2a988e413a05b55d98a65e8dc
Joseph D. Marhee <jmarhee@interiorae.com>
2016-06-28 12:40:08 -0500

    added registrator container; points to manager-primary consul instance

--- swarm.tf
+++ swarm.tf
@@ -32,7 +32,8 @@ resource "digitalocean_droplet" "manager-primary" {
       "export PATH=$PATH:/usr/bin",
       "service docker start",
       "docker run --restart=unless-stopped -d -h consul00 --name consul00 -v /mnt:/data -p ${self.ipv4_address_private}:8300:8300 -p ${self.ipv4_address_private}:8301:8301 -p ${self.ipv4_address_private}:8301:8301/udp -p ${self.ipv4_address_private}:8302:8302 -p ${self.ipv4_address_private}:8302:8302/udp -p ${self.ipv4_address_private}:8400:8400 -p ${self.ipv4_address_private}:8500:8500 -p 172.17.0.1:53:53/udp progrium/consul -server -advertise ${self.ipv4_address_private} -bootstrap-expect 3",
-      "docker run --restart=unless-stopped -h mgr00 --name mgr00 -d -p 3375:2375 swarm manage --replication --advertise ${self.ipv4_address_private}:3375 consul://${self.ipv4_address_private}:8500/"
+      "docker run --restart=unless-stopped -h mgr00 --name mgr00 -d -p 3375:2375 swarm manage --replication --advertise ${self.ipv4_address_private}:3375 consul://${self.ipv4_address_private}:8500/",
+      "docker run -d --name registrator-00 -h registrator-00 -v /var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator:latest consul://${self.ipv4_address_private}:8500"
     ]
   }
 }
@@ -60,7 +61,8 @@ resource "digitalocean_droplet" "manager-replica" {
       "export PATH=$PATH:/usr/bin",
       "service docker start",
       "docker run --restart=unless-stopped -d -h consul${count.index} --name consul${count.index} -v /mnt:/data -p ${self.ipv4_address_private}:8300:8300 -p ${self.ipv4_address_private}:8301:8301 -p ${self.ipv4_address_private}:8301:8301/udp -p ${self.ipv4_address_private}:8302:8302 -p ${self.ipv4_address_private}:8302:8302/udp -p ${self.ipv4_address_private}:8400:8400 -p ${self.ipv4_address_private}:8500:8500 -p 172.17.0.1:53:53/udp progrium/consul -server -advertise ${self.ipv4_address_private} -join ${digitalocean_droplet.manager-primary.ipv4_address_private}",
-      "docker run --restart=unless-stopped -h mgr${count.index} --name mgr${count.index} -d -p 3375:2375 swarm manage --replication --advertise ${self.ipv4_address_private}:3375 consul://${self.ipv4_address_private}:8500/"
+      "docker run --restart=unless-stopped -h mgr${count.index} --name mgr${count.index} -d -p 3375:2375 swarm manage --replication --advertise ${self.ipv4_address_private}:3375 consul://${self.ipv4_address_private}:8500/",
+      "docker run -d --name registrator-${count.index} -h registrator-${count.index} -v /var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator:latest consul://${digitalocean_droplet.manager-primary.ipv4_address_private}:8500"
     ]
   }
 }
@@ -88,7 +90,8 @@ resource "digitalocean_droplet" "node" {
       "export PATH=$PATH:/usr/bin",
       "service docker start",
       "docker run --restart=unless-stopped -d -h consul-agt${count.index} --name consul-agt${count.index} -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 8600:8600/udp progrium/consul -rejoin -advertise ${self.ipv4_address_private} -join ${digitalocean_droplet.manager-primary.ipv4_address_private}",
-      "docker run -d swarm join --advertise=${self.ipv4_address_private}:2375 consul://${digitalocean_droplet.manager-primary.ipv4_address_private}:8500/"
+      "docker run -d swarm join --advertise=${self.ipv4_address_private}:2375 consul://${digitalocean_droplet.manager-primary.ipv4_address_private}:8500/",
+      "docker run -d --name registrator-${count.index} -h registrator-${count.index} -v /var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator:latest consul://${digitalocean_droplet.manager-primary.ipv4_address_private}:8500"
     ]
   }
 }
